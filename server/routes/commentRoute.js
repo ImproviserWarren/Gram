b1f794080211add2dc12877805368fde43f5d59b
Snkenim <snekim288@gmail.com>
2024-11-23 09:56:16 +0800

    Remove deleted files

--- server/routes/commentRoute.js
+++
@@ -1,8 +0,0 @@
-const { Router } = require("express")
-const createComment = require("../controllers/commentController")
-
-const commentRoute = Router()
-
-commentRoute.post('/createComment', createComment)
-
-module.exports = commentRoute
--- server/routes/likeRoute.js
+++
@@ -1,9 +0,0 @@
-const { Router } = require("express")
-const { spreadLike, validateLiker } = require("../controllers/likeController")
-
-const likeRoute = Router()
-
-likeRoute.put('/likePost', spreadLike, validateLiker)
-
-
-module.exports = likeRoute
